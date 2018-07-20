2def5cf50e3f1cfeb74a9d5bc44031f09396ecfa
Jo Surzyn <josurzyn@gmail.com>
2018-07-20 20:57:48 +0200

    feat: Set up basic routing

--- src/OpenSearch.js
+++ src/OpenSearch.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function OpenSearch() {
   return (
     <div className="open-search">
-      <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+      <Link to="/search">Add a book</Link>
     </div>
   )
 }
